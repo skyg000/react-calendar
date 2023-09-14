38d5d8c6c7369c4da4beb1b6a2b2f39b508ccbe7
skyg000 <skyg00700@gmail.com>
2023-09-14 12:50:45 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import background from './background.jpg';
+
 import './App.scss';
 import Home from './comp/Home';
+import Calendar1 from './comp/Calendar1'
 import {Link,BrowserRouter,Route,Routes} from 'react-router-dom';
 function App() {
   return (
+
+
+
     <BrowserRouter>
       <main>
-        <img src={background} className='back'/>
-        <h1> 일정관리 </h1>
-        <Link to="/home" className='login'> 로그인 </Link>
-      </main>
       <Routes>
-        <Route path='/home'element={<Home/>}/>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/calendar1'element={<Calendar1/>}/>
       </Routes>
+      
+      </main>
     </BrowserRouter>
   );
 }
